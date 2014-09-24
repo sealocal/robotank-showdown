4d00d03a47faad749d455fe62866b68575453c24
Philip Nguyen <supertaru@gmail.com>
2014-09-24 15:23:30 -0700

    Duck is back

---
+++ duck.rb
@@ -0,0 +1,18 @@
+require 'rrobots'
+
+class Duck
+  include Robot
+
+  def tick events
+    turn 2
+    fire 3 unless events['robot_scanned'].empty?
+  end
+  # def tick events
+  #   turn_radar 1 if time == 0
+  #   turn_gun 30 if time < 3
+  #   accelerate 1
+  #   turn 2
+  #   fire 3 unless events['robot_scanned'].empty?
+  # end
+
+end
